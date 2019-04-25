d1b523829612d286d1a61e95d836d9d023861605
premki <prem@deepcompute.com>
2019-04-25 07:36:47 +0000

    added haproxy handler to logagg ref #87

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name="logagg",
-    version="0.3.3",
+    version="0.3.4",
     description="logs aggregation framework",
     keywords="logagg",
     author="Deep Compute, LLC",
